5ccfcd6fb8e9209ff093ddac85d252707c11ca58
Erivelton Rodrigues <eriveltonarruda3000@gmail.com>
2024-08-18 19:29:19 -0300

    Update de anotações e adição do Exercício

--- Módulo 1 - Primeiros Passos/3. Variáveis/assets/js/script.js
+++ Módulo 1 - Primeiros Passos/3. Variáveis/assets/js/script.js
@@ -7,3 +7,10 @@ console.log(idade);
 // Não colocamos a variável dentro de string
 console.log("nome");
 console.log(idade);
+
+// Exercício da Aula: Crie duas variáveis, uma com sua cidade e outra com seu estado e imprima no console
+let cidade = "Gravatá";
+let estado = "Pernambuco";
+
+console.log(cidade);
+console.log(estado);
