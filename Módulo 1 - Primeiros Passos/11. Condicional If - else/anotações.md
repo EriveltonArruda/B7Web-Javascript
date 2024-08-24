716e52575e094aebc3552f4bc82f23c79a499c34
Erivelton Rodrigues <eriveltonarruda3000@gmail.com>
2024-08-24 14:20:31 -0300

    Melhorando anotações

--- Módulo 1 - Primeiros Passos/11. Condicional If - else/anotações.md
+++ Módulo 1 - Primeiros Passos/11. Condicional If - else/anotações.md
@@ -1,5 +1,17 @@
 Condicionais são ações que queremos que o código faça a partir de tal condição, um bom exemplo é o acelerador de um carro, um carro só anda SE o acelerador for acionado.
 
+## Parâmetros do if/else
+
+um if e else funciona exatamente dessa maneira:
+SE (tal condição for verdadeira) {
+execute isso
+}
+CASO CONTRÁRIO ou SE NÂO {
+execute isso
+}
+
+Colocamos a condição dentro de parênteses e se for verdadeira, ela vai executar a ação que definirmos, essa ação do if só será executada se for verdadeira, caso contrário ela executa a ação do else.
+
 ## Lista de condicionais do if
 
 > (maior)
--- Módulo 1 - Primeiros Passos/11. Condicional If - else/assets/js/script.js
+++ Módulo 1 - Primeiros Passos/11. Condicional If - else/assets/js/script.js
@@ -1,27 +1,14 @@
-// Parâmetros de if/else
-/*
-um if e else funciona exatamente dessa maneira:
-SE (tal condição for verdadeira) {
-	execute isso 
-} 
-CASO CONTRÁRIO ou SE NÂO {
-	execute isso
-}
-
-Colocamos a condição dentro de parênteses e se for verdadeira, ela vai executar a ação que definirmos, essa ação do if só será executada se for verdadeira, caso contrário ela executa a ação do else.
-*/
-
 // Exemplo de maior de idade //
 /*
 let idade = 19;
 
 if (idade >= 18) {
   console.log(
-    `Maior de idade é quem tem 18 anos, sua idade é: ${idade}, logo, você é MAIOR de Idade`
+    `Maior de idade é quem tem 18 anos, sua idade é: ${idade}, então você é MAIOR de Idade`
   );
 } else {
   console.log(
-    `Maior de idade é quem tem 18 anos, sua idade é: ${idade}, logo, você é MENOR de Idade`
+    `Maior de idade é quem tem 18 anos, sua idade é: ${idade}, então você é MENOR de Idade`
   );
 }
 */
@@ -29,13 +16,13 @@ if (idade >= 18) {
 // Comparação em if
 let idade = 14;
 
-// se usa dois sinais de iguais para comparar se os valores são iguais, não se usa um igual (=) porque esse é um sinal de atribuição de valor à variável.
+// se usa dois sinais de igual para comparar se os valores são iguais, não se usa um igual (=) porque esse é um sinal de atribuição de valor à variável.
 if (idade == 18) {
   console.log(
-    `Maior de idade é quem tem 18 anos, sua idade é: ${idade} logo, você é MAIOR de Idade`
+    `Maior de idade é quem tem 18 anos, sua idade é: ${idade}, então você é MAIOR de Idade`
   );
 } else {
   console.log(
-    `Maior de idade é quem tem 18 anos, sua idade é: ${idade} logo, você é MENOR de Idade`
+    `Maior de idade é quem tem 18 anos, sua idade é: ${idade}, então você é MENOR de Idade`
   );
 }
