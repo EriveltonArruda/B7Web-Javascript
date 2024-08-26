28ed41707d2daa663f08161718a26a6933456de8
Erivelton Rodrigues <eriveltonarruda3000@gmail.com>
2024-08-26 19:38:29 -0300

    Revisando anotações

--- Módulo 1 - Primeiros Passos/12. Condicional == e ===/assets/js/script.js
+++ Módulo 1 - Primeiros Passos/12. Condicional == e ===/assets/js/script.js
@@ -28,19 +28,19 @@ if (idade2 === 20) {
 let idade2 = "20";
 
 if (idade2 == 20) {
-  idade += 15; // também poderia ser idade = idade + 15;
-  console.log(`Idade atualizada com sucesso! Sua idade agora é ${idade}`);
+  idade2 += 15; // também poderia ser idade2 = idade2 + 15;
+  console.log(`Idade atualizada com sucesso! Sua idade agora é ${idade2}`);
 }
 
 // Aqui não entraria no if porque o TIPO da variável está como string e na comparação === está com o TIPO em número, logo, são tipos diferentes e a condição não é executada.
 let idade3 = "20";
 
 if (idade3 === 20) {
-  idade += 15;
-  console.log(`Idade atualizada com sucesso! Sua idade agora é ${idade}`);
+  idade3 += 15;
+  console.log(`Idade atualizada com sucesso! Sua idade agora é ${idade3}`);
 }
 
-// OOperações do jeito certo //
+// Operações do jeito certo //
 let idade = 20;
 
 // tanto assim
