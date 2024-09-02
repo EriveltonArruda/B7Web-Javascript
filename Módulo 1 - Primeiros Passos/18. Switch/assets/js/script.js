500a8e3581f70239cb3c553c1a5de11b8876b12e
Erivelton Rodrigues <eriveltonarruda3000@gmail.com>
2024-09-02 11:55:38 -0300

    Aula de Switch

---
+++ Módulo 1 - Primeiros Passos/18. Switch/assets/js/script.js
@@ -0,0 +1,24 @@
+// Exemplo da profissão.
+// A cor da camisa será escolhida com base na profissão escolhida.
+let profession = "policial";
+
+console.log(`Profissão: ${ profession }`);
+
+// Verifico a variável e cada case terá seu código a ser executado, no lugar de  chaves, são utilizados dois pontos (:)
+switch (profession) {
+  case "fiscal":
+    console.log("Sua camisa será VERDE");
+    break;
+  case "bombeiro":
+    console.log("Sua camisa será VERMELHA");
+    break;
+  case "policial":
+    console.log("Sua camisa será AZUL");
+    break;
+  // precisamos de um padrão além das opções
+  // porque caso nenhuma condição seja escolhida, a padrão assumirá
+  default:
+    console.log("Sua camisa será PRETA");
+}
+
+// Um adendo importante é que é bom colocar sempre um break no final de cada "case" isso faz com que o código entenda que aquela condição termina ali, caso não tivesse o break, o código iria executar tudo de uma vez.
