79e991ea7024da26df4da35ed75937bfc7b93b5d
Erivelton Rodrigues <eriveltonarruda3000@gmail.com>
2024-09-03 17:45:22 -0300

    Commit Inicial

---
+++ Módulo 2 - Javascript Básico/1. Declarando uma função/assets/js/script.js
@@ -0,0 +1,7 @@
+// O que é uma função?
+
+/* 
+Funções são trechos de códigos reutilizáveis, onde criamos uma função, deixamos ela guardada e podemos reutiliza-la posteriormente.
+
+Toda função precisa de um dado de entrada e no final ela sempre precisa retornar algo, imagine por exemplo: uma função que transforma textos normais em letras maiúsculas, dentro dessa função tem os códigos que vão fazer a ação de transformar textos minúsculos em maiúsculos, só temos que criar ela, definir dentro dela a ação que ela executará e podemos chamar ela quando quisermos, dessa forma não precisamos criar o código toda vez que formos transformar o texto, criamos uma função pra isso e usamos essa função sempre que necessário.
+*/
