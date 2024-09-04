10faa2512707d265eca5312c084408a2f3ad681a
Erivelton Rodrigues <eriveltonarruda3000@gmail.com>
2024-09-04 17:49:02 -0300

    Aula Passando parâmetros em funções

---
+++ Módulo 2 - Javascript Básico/3. Passando parâmetros em funções/assets/js/script.js
@@ -0,0 +1,27 @@
+// Função de soma
+// passo os parâmetros da função (n1, n2)
+function somar(n1, n2) {
+  /* Crio uma variável que receberá os dados dos parâmetros e armazenará o 
+  resultado da operação (nesse caso é a soma) nessa variável. */
+  let resultado = n1 + n2;
+  console.log(`Resultado: ${ resultado }`);
+}
+
+/* Chamando a variável e passando os parâmetros para que a função faça
+o cálculo */
+somar(15, 30);
+
+// Processo realizado pela função soma 
+/*
+n1 vai ser 15
+n2 vai ser 30
+E dentro da função será feita a soma.
+*/
+
+// Exemplo de função de nomes
+function nomeCompleto(nome, sobrenome) {
+  console.log(`${ nome } ${ sobrenome }`);
+}
+
+nomeCompleto("Erivelton", "Rodrigues");
+nomeCompleto("João", "Silva");
