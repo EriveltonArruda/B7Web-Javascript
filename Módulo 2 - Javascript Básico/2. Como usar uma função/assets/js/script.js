e8a19b4ad725ddd9b87c7a5db32a922c8422307a
Erivelton Rodrigues <eriveltonarruda3000@gmail.com>
2024-09-04 07:50:49 -0300

    Commit inicial

--- Módulo 2 - Javascript Básico/2. Como usar uma função/assets/js/script.js
+++ Módulo 2 - Javascript Básico/2. Como usar uma função/assets/js/script.js
@@ -1,16 +1,9 @@
-// Para criar uma função usamos o comando function e definimos um nome
-// Com relação ao nome, não pode ter caracteres especiais nem espaço, se for     criar uma função com nome composto, utilize o camelCase.
-function galaxia() {
-  // corpo da função que terá os códigos que farão a ação.
-}
-
-// utilizando camelCase (primeira letra minúscula e segunda maiúscula)
-function megaGalaxia() {
-
-}
-
-// Exemplo da gravidade
+// Declarando uma função (criando)
 function gravidade() {
   console.log("A gravidade do planeta é: ");
   console.log(9.8);
 }
+
+// Usando a função (chamando para ser usada)
+gravidade();
+gravidade();
