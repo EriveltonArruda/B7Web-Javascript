f6d8649c0d1af4fcb59df43f08c5f0fb9bdc6d86
Erivelton Rodrigues <eriveltonarruda3000@gmail.com>
2024-09-05 20:45:11 -0300

    Aula retorno da função

---
+++ Módulo 2 - Javascript Básico/4. Retorno da função/assets/js/script.js
@@ -0,0 +1,10 @@
+// Retorno da função
+function nomeCompleto(nome, sobrenome) {
+  return `${nome} ${sobrenome}`;
+}
+
+// Armazenando o retorno da função na variável completo
+let completo = nomeCompleto("Erivelton", "Rodrigues");
+console.log(`Nome completo: ${completo}`);
+
+// ENTRADA -> PROCESSAMENTO -> SAIDA  
