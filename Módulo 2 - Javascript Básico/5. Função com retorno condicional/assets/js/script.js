8f624476bec6f517875c973666fd9ed50bfee387
Erivelton Rodrigues <eriveltonarruda3000@gmail.com>
2024-09-06 11:26:27 -0300

    Revisão da aula

--- Módulo 2 - Javascript Básico/5. Função com retorno condicional/assets/js/script.js
+++ Módulo 2 - Javascript Básico/5. Função com retorno condicional/assets/js/script.js
@@ -1,3 +1,4 @@
+// Podemos usar condições dentro de funções
 function maiorDeIdade(idade) {
   if (idade >= 18) {
     return true;
@@ -6,9 +7,12 @@ function maiorDeIdade(idade) {
   }
 }
 
+// Outra forma de chamar a função, passando a variável que contém o valor, como parâmetro da função
 let idade = 65;
 let verificacao = maiorDeIdade(idade);
 
+
+// Também posso fazer uma verificação da variável que tem a função como valor
 if (verificacao) {
   console.log("É maior de idade");
 } else {
