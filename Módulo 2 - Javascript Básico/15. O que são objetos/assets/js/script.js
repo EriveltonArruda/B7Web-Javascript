05e6b6284589d3051b74715538c1c77fb5002b7c
Erivelton Rodrigues <eriveltonarruda3000@gmail.com>
2024-09-15 11:40:52 -0300

    Aula de Objetos

---
+++ Módulo 2 - Javascript Básico/15. O que são objetos/assets/js/script.js
@@ -0,0 +1,33 @@
+// Forma de criar variável normal
+let nome = "Erivelton";
+
+// Array de variáveis
+// Erivelton é a chave [0], Pedro é a chave [1]
+let nomes = ["Erivelton", "Pedro"];
+
+// Objeto com características de um personagem de RPG
+let personagem = {
+  nome: "Erivelton",
+  idade: 32,
+  País: "Brasil",
+  // Array dentro de objetos
+  olhos: ["Preto", "Azul"],
+  características: {
+    // Não posso colocar caracteres especiais como "ç"
+    forca: 20,
+    magia: 5,
+    stamina: 15,
+  },
+};
+
+// Selecionando um item dentro de um array a partir da sua posição
+console.log(nomes[0]);
+
+/* Para selecionar um item de um objeto digitamos o nome do objeto (propriedade) seguido da sua chave depois de um ponto(.) */
+console.log(`${personagem.nome} tem ${personagem.idade} anos`);
+
+// Selecionando um objeto dentro de outro objeto
+console.log(personagem.características.magia);
+
+// Selecionando o valor do array que está dentro do objeto
+console.log(personagem.olhos[1]);
