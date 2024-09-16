5b848e8a5e5d5b75dba42ee9fbbc6dc009bb39de
Erivelton Rodrigues <eriveltonarruda3000@gmail.com>
2024-09-16 11:52:59 -0300

    Função dentro de objeto

---
+++ Módulo 2 - Javascript Básico/17. Função dentro de objeto/assets/js/script.js
@@ -0,0 +1,40 @@
+// Objeto pessoa
+let pessoa = {
+  nome: "Erivelton",
+  sobrenome: "Rodrigues",
+  idade: 32,
+};
+
+// Resultado: Nome Completo: Erivelton Rodrigues
+console.log(`Nome Completo: ${ pessoa.nome } ${ pessoa.sobrenome }`);
+
+// Função dentro de objeto
+let pessoa2 = {
+  nome: "Erivelton",
+  sobrenome: "Rodrigues",
+  idade: 32,
+  nomeCompleto: function () {
+    // Chamando o item do próprio objeto, nesse caso nome e sobrenome, então vai  chamar o nome e o sobrenome DESTE (this) objeto, nesse caso é pessoa
+    return `${ this.nome } ${ this.sobrenome }`;
+  },
+};
+
+// Para chamar ele no console fazemos assim:
+/* Apesar dessa função estar dentro de um objeto, podemos usar a função 
+normalmente. */
+console.log(pessoa2.nomeCompleto()); // Resultado: Erivelton Rodrigues
+
+/* O this não funciona com arrow function
+let pessoa = {
+  nome: "Erivelton",
+  sobrenome: "Rodrigues",
+  idade: 32,
+  // Arrow function não funciona o this
+  nomeCompleto: () => {
+    return `${this.nome} ${this.sobrenome}`;
+  },
+};
+
+console.log(pessoa.nomeCompleto());
+// Resultado: undefined undefined
+*/
