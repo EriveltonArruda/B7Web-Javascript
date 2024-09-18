6adac8541dab080bbb60b32962d87d7e4b02c716
Erivelton Rodrigues <eriveltonarruda3000@gmail.com>
2024-09-17 21:27:55 -0300

    Exercício de Loops

---
+++ Módulo 2 - Javascript Básico/21. Exercício de loops #8/assets/js/script.js
@@ -0,0 +1,17 @@
+// 1. Faça um loop que mostre todas as frutas
+let fruits = ["Maçã", "Uva", "Banana"];
+for (let fruit of fruits) {
+  console.log(fruit);
+}
+
+// Assim também funciona
+for (let i in fruits) {
+  console.log(fruits[i]);
+}
+
+// 2. conte de 1 até 100 através de um WHILE
+let n = 1;
+while (n <= 100) {
+  console.log(n);
+  n++;
+}
