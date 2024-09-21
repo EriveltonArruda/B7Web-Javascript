84d42a5933981a2ad09122b18fe27a8622be6d25
Erivelton Rodrigues <eriveltonarruda3000@gmail.com>
2024-09-21 12:25:53 -0300

    Selecionando elementos 2

---
+++ Módulo 3 - Eventos e manipulação de DOM/3. Selecionando elementos 2/assets/js/script.js
@@ -0,0 +1,18 @@
+// Selecionando pelo ID
+document.getElementById("#teste");
+
+// Selecionando pela classe
+document.getElementsByClassName(".botao");
+
+// Forma mais aberta de selecionar
+// Pelo ID
+document.querySelector("#teste");
+
+// pela classe
+document.querySelector(".botao");
+
+// Selecionando todos os elementos
+document.querySelectorAll("li");
+
+// Selecionando os elementos por cascata
+document.querySelectorAll("#teste ul li");
