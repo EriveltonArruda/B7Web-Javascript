7d5e966edadce8db2048e5e665058a3312efc466
Erivelton Rodrigues <eriveltonarruda3000@gmail.com>
2024-09-25 17:48:29 -0300

    Revisando o código

--- Módulo 3 - Eventos e manipulação de DOM/5. Manipulando elementos 1/assets/js/script.js
+++ Módulo 3 - Eventos e manipulação de DOM/5. Manipulando elementos 1/assets/js/script.js
@@ -13,10 +13,17 @@ function clicou() {
 
   // Se fosse feito dessa forma, não funcionaria tag html, porque seria passado um texto
   // ul.children[0].innerText = "Item <strong>alterado</strong>";
+
+  // outerHTML
+  console.log(ul.outerHTML);
+
+  // Alterando com outerHTML
+  ul.outerHTML = "<strong>Alterado!</strong>"
 }
 
 // por ser innerHTML, ele aceita tags de html
 
 // O princípio do innerText é o mesmo do innerHTML
 // A diferença é que o Text interpreta tudo como texto
-// O innerHTML consegue interpretar tags dentro dele e usa essas tags também
+// O innerHTML consegue interpretar tags dentro dele, assim ele altera o texto
+// como a tag alteraria o elemento no HTML
