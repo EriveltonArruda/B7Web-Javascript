e383fbfd33749fb635f5738bc6ac183a6d107c68
Erivelton Rodrigues <eriveltonarruda3000@gmail.com>
2024-09-25 11:54:59 -0300

    Manipulando Elementos 1

---
+++ Módulo 3 - Eventos e manipulação de DOM/5. Manipulando elementos 1/assets/js/script.js
@@ -0,0 +1,22 @@
+function clicou() {
+  const teste = document.querySelector("#teste");
+  const ul = teste.querySelector("ul");
+
+  // Alterando o elemento com o innerHTML
+  //ul.innerHTML = "<li>Item alterado!</li>";
+
+  // Adicionando mais um li com o innerHTML
+  ul.innerHTML += "<li>Item alterado!</li>";
+
+  // Outra forma de alterar
+  // ul.children[0].innerHTML = "Item alterado!";
+
+  // Se fosse feito dessa forma, não funcionaria tag html, porque seria passado um texto
+  // ul.children[0].innerText = "Item <strong>alterado</strong>";
+}
+
+// por ser innerHTML, ele aceita tags de html
+
+// O princípio do innerText é o mesmo do innerHTML
+// A diferença é que o Text interpreta tudo como texto
+// O innerHTML consegue interpretar tags dentro dele e usa essas tags também
