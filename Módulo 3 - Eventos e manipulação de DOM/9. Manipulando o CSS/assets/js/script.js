ef9c8587a5c45a3c4130bc1297fc99c6addc27b0
Erivelton Rodrigues <eriveltonarruda3000@gmail.com>
2024-09-28 11:41:02 -0300

    Manipulando CSS

---
+++ Módulo 3 - Eventos e manipulação de DOM/9. Manipulando o CSS/assets/js/script.js
@@ -0,0 +1,8 @@
+function clicou() {
+  const li = document.querySelector("li");
+
+  li.style.backgroundColor = "#00f";
+  // Se não existir a propriedade no elemento
+  // Ela será adicionada
+  li.style.fontSize = "20px";
+}
