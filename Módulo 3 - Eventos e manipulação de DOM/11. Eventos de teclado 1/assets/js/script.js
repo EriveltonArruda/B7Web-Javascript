3c9648cd54434c134f8a6455919eca964b1b550a
Erivelton Rodrigues <eriveltonarruda3000@gmail.com>
2024-09-29 20:57:19 -0300

    Eventos de teclado 1

---
+++ Módulo 3 - Eventos e manipulação de DOM/11. Eventos de teclado 1/assets/js/script.js
@@ -0,0 +1,17 @@
+// funções usadas nos onkey do HTML
+function apertou() {
+  console.log("APERTOU!");
+}
+
+function segurou() {
+  console.log("SEGUROU!");
+}
+
+function soltou() {
+  console.log("SOLTOU!");
+}
+
+// Adicionando evento pelo JS, aqui não usa o ON antes do key.
+// 1º parâmetro = o evento que quero
+// 2º parâmetro = a função que será executada
+document.addEventListener('keyup', soltou);
