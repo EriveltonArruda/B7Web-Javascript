a18e1d0b5b78746355971cf330bdf813e54df23c
Erivelton Rodrigues <eriveltonarruda3000@gmail.com>
2024-09-30 17:22:54 -0300

    O que é OO

--- Módulo 3 - Eventos e manipulação de DOM/9. Manipulando o CSS/assets/js/script.js
+++ Módulo 3 - Eventos e manipulação de DOM/9. Manipulando o CSS/assets/js/script.js
@@ -1,6 +1,8 @@
 function clicou() {
   const li = document.querySelector("li");
 
+  // Quando uma propriedade tem traço - 
+  // utilizamos camelCase no DOM
   li.style.backgroundColor = "#00f";
   // Se não existir a propriedade no elemento
   // Ela será adicionada
---
+++ Módulo 4 - Objetos e Orientação à objetos/1. O que é OO/assets/js/script.js
@@ -0,0 +1,13 @@
+/*
+Na programação orientada a objetos, quem guia a criação dos códigos são os objetos.
+
+Objeto nada mais é do que uma simulação de um objeto do mundo real, Um personagem de um jogo de RPG é um ótimo exemplo, pois ele tem características:
+
+Personagem = {
+  nome: "Erivelton",
+  idade: "33 Anos",
+  Classe: "mago"
+}
+
+Na programação orientada a objetos, um objeto se comunica com o outro.
+*/
