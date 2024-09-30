050374b82961a694c74394ecf2670e7c11efe8fa
Erivelton Rodrigues <eriveltonarruda3000@gmail.com>
2024-09-30 09:24:48 -0300

    Eventos de teclado 2

---
+++ Módulo 3 - Eventos e manipulação de DOM/12. Eventos de teclado 2/assets/js/script.js
@@ -0,0 +1,22 @@
+// O parâmetro event tem detalhes do evento
+// pode colocar o nome que quiser, muita gente só coloca o (e)
+// Ele é um objeto
+function soltou(e) {
+
+  // code mostra em detalhes o que foi apertado (leftShift ou RightShift por exemplo)
+  console.log(e.code);
+  // key mostra não distingue LeftShift de Shift por exemplo
+  console.log(e.key);
+
+  console.log(`TECLA APERTADA: ${ e.code }`);
+  // O shiftKey é um boolean, 
+  // ele nos mostra se o shift foi apertado junto com a tecla
+  console.log(`SHIFT? ${ e.shiftKey }`);
+  // a lógica do shift serve para o ctrl e o alt também
+  console.log(`CTRL? ${ e.ctrlKey }`);
+  console.log(`ALT? ${ e.altKey }`);
+}
+
+// o exemplo será mostrado no input.
+const input = document.querySelector('input');
+input.addEventListener('keyup', soltou);
