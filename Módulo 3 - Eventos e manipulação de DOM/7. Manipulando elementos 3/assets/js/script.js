c1f40f21b532c84a0c7fa0825246ebbbf8b96d8f
Erivelton Rodrigues <eriveltonarruda3000@gmail.com>
2024-10-29 21:02:28 -0300

    .

--- Módulo 3 - Eventos e manipulação de DOM/7. Manipulando elementos 3/assets/js/script.js
+++ Módulo 3 - Eventos e manipulação de DOM/7. Manipulando elementos 3/assets/js/script.js
@@ -5,11 +5,11 @@ function clicou() {
   // adiciona texto depois do elemento
   ul.after("Texto qualquer");
 
-  // Adiciona texto antes do elemento
-  ul.before("Texto qualquer");
-
   // não aceita elemento, apenas texto
   ul.after("<button>Botão</button>");
+
+  // Adiciona texto antes do elemento
+  ul.before("Texto qualquer");
 
   // Criando um botão para usar o after ou o before nele
   const newButton = document.createElement("button");
@@ -18,17 +18,17 @@ function clicou() {
   // Usando o botão antes do ul
   ul.before(newButton);
 
-  // Criando uma lista com DOM
+  // Criando uma lista com DOM e deixando na memória
   let newUl = document.createElement("ul");
 
-  // são vários itens, então uso o for
+  // criando "li" para a nova lista
   for (let i = 0; i < 5; i++) {
     // Crio o li
     let newLi = document.createElement("li");
     // Adiciono o texto a ele (pode usar o append ou innerHTML também)
-    // mas é bom só usar o innerHTML quando realmente precisar
-    // passar um elemento de HTML, caso contrário, use Text
-    newLi.innerText = `Item da lista nova ${ i }`;
+    // mas é bom só usar o innerHTML quando realmente 
+    // precisar passar um elemento de HTML, caso contrário, use Text
+    newLi.innerText = `Item da lista nova ${i}`;
     // Se quiser que o item comece no 1 no lugar do 0, faça isso no for:
     // newLi.innerHTML = `Item da lista nova ${ i + 1 }`;
     // Adiciono esse li na lista nova
