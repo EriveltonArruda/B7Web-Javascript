50d73e87a76a0529eb0bebe2c0ad63ef3c666910
Erivelton Rodrigues <eriveltonarruda3000@gmail.com>
2024-10-31 17:48:36 -0300

    Melhorando anotações

--- Módulo 1 - Primeiros Passos/11. Condicional If - else/assets/js/script.js
+++ Módulo 1 - Primeiros Passos/11. Condicional If - else/assets/js/script.js
@@ -16,13 +16,14 @@ if (idade >= 18) {
 // Comparação em if
 let idade = 14;
 
-// se usa dois sinais de igual para comparar se os valores são iguais, não se usa um igual (=) porque esse é um sinal de atribuição de valor à variável.
+// se usa dois sinais de igual para comparar se os valores são iguais, 
+// não se usa um igual (=) porque esse é um sinal de atribuição de valor à variável.
 if (idade == 18) {
   console.log(
-    `Maior de idade é quem tem 18 anos, sua idade é: ${idade}, então você é MAIOR de Idade`
+    `Maior de idade é quem tem 18 anos, sua idade é: ${ idade }, então você é MAIOR de Idade`
   );
 } else {
   console.log(
-    `Maior de idade é quem tem 18 anos, sua idade é: ${idade}, então você é MENOR de Idade`
+    `Maior de idade é quem tem 18 anos, sua idade é: ${ idade }, então você é MENOR de Idade`
   );
 }
--- Módulo 1 - Primeiros Passos/3. Variáveis/anotações.md
+++ Módulo 1 - Primeiros Passos/3. Variáveis/anotações.md
@@ -1,6 +1,8 @@
+## Criar variáveis
+
 para criar uma variável no Javascript utilizamos let ou const (Não use var, é antigo), onde let é utilizado para criar variáveis que podem ter seus valores re-atribuídos posteriormente e em const não tem seu valor alterado, pois é uma constante.
 
-<!-- Não colocamos a variável dentro de aspas, porque será exibido no console o nome que está dentro das aspas e não o valor da variável. -->
+Não colocamos a variável dentro de aspas, porque será exibido no console o nome que está dentro das aspas e não o valor da variável.
 
 console.log("nome");
 console.log(idade);
