39bced9baf9bf39bf0bd157ff37b85972f3dcdda
Erivelton Rodrigues <eriveltonarruda3000@gmail.com>
2024-11-03 21:03:36 -0300

    .

--- Módulo 4 - Objetos e Orientação à objetos/1. O que é OO/assets/js/script.js
+++ Módulo 4 - Objetos e Orientação à objetos/1. O que é OO/assets/js/script.js
@@ -2,6 +2,7 @@
 Na programação orientada a objetos, quem guia a criação dos códigos são os objetos.
 
 Objeto nada mais é do que uma simulação de um objeto do mundo real, Um personagem de um jogo de RPG é um ótimo exemplo, pois ele tem características:
+*/
 
 Personagem = {
   nome: "Erivelton",
@@ -9,5 +10,4 @@ Personagem = {
   Classe: "mago"
 }
 
-Na programação orientada a objetos, um objeto se comunica com o outro.
-*/
+// Na programação orientada a objetos, um objeto se comunica com o outro.
