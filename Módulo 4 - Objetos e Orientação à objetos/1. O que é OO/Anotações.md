c5b94d7e6aae0381efd3b5afe9927900710a8eb7
Erivelton Rodrigues <eriveltonarruda3000@gmail.com>
2024-11-04 11:53:49 -0300

    Começando Orientação a Objetos

---
+++ Módulo 4 - Objetos e Orientação à objetos/1. O que é OO/Anotações.md
@@ -0,0 +1,7 @@
+## Orientação a objetos
+
+Na programação orientada a objetos, quem guia a criação dos códigos são os objetos.
+
+Objeto nada mais é do que uma "simulação de um objeto real do mundo real", Um personagem de um jogo de RPG é um ótimo exemplo, pois ele tem características.
+
+Na programação orientada a objetos, um objeto se comunica com o outro.
--- Módulo 4 - Objetos e Orientação à objetos/1. O que é OO/assets/js/script.js
+++ Módulo 4 - Objetos e Orientação à objetos/1. O que é OO/assets/js/script.js
@@ -1,13 +1,6 @@
-/*
-Na programação orientada a objetos, quem guia a criação dos códigos são os objetos.
-
-Objeto nada mais é do que uma simulação de um objeto do mundo real, Um personagem de um jogo de RPG é um ótimo exemplo, pois ele tem características:
-*/
-
+// Exemplo do personagem de RPG
 Personagem = {
   nome: "Erivelton",
   idade: "33 Anos",
   Classe: "mago"
 }
-
-// Na programação orientada a objetos, um objeto se comunica com o outro.
