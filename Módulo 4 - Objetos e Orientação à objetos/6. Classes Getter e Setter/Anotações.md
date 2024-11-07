a8e7bc378c71d7d1d3ff0facd352cbb2667ea095
Erivelton Rodrigues <eriveltonarruda3000@gmail.com>
2024-11-07 17:07:04 -0300

    Classes: getters e setters

---
+++ Módulo 4 - Objetos e Orientação à objetos/6. Classes Getter e Setter/Anotações.md
@@ -0,0 +1,13 @@
+## O que são
+
+Os métodos getters e setters no JavaScript são usados para acessar e modificar os atributos de uma classe:
+
+```
+Getter
+Retorna o valor de um campo. A sintaxe de get associa uma propriedade de um objeto a uma função que é chamada quando essa propriedade é acessada. `
+```
+
+```
+Setter
+Atribui um valor a um campo. O setter pode ser usado para executar uma função sempre que se tenta mudar uma propriedade específica
+```
---
+++ Módulo 4 - Objetos e Orientação à objetos/6. Classes Getter e Setter/assets/js/script.js
@@ -0,0 +1,45 @@
+class Person {
+  // Geralmente se coloca underline antes da variável
+  // quando estamos trabalhando com get e set
+  _age = 0;
+  steps = 0;
+
+  constructor (firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  takeAStep() {
+    this.steps++;
+  }
+
+  // usamos get para combinar variáveis também
+  get fullName() {
+    return `${ this.firstName } ${ this.lastName }`
+  }
+
+  get age() {
+    return this._age;
+  }
+
+  // definindo a idade
+  set age(x) {
+    // também podemos fazer a verificação
+    if (typeof x == "number") {
+      this._age = x;
+    } else {
+      console.log("Apenas Números!");
+    }
+  }
+}
+
+let p1 = new Person("João", "Silva");
+let p2 = new Person("Maria", "Santos");
+let p3 = new Person("Pedro", "Duarte");
+
+// usando o set
+p1.age = 20;
+
+// O age no template está executando a função age do get
+// Assim como o fullName está executando a função fullName do get
+console.log(`${ p1.fullName } tem ${ p1.age } anos.`);
