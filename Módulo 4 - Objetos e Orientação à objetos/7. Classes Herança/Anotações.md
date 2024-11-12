b6d55b0a804d66964839f5d800f5b83f82e9e655
Erivelton Rodrigues <eriveltonarruda3000@gmail.com>
2024-11-12 17:36:28 -0300

    Classes: Herança

--- Módulo 4 - Objetos e Orientação à objetos/7. Classes Herança/Anotações.md
+++ Módulo 4 - Objetos e Orientação à objetos/7. Classes Herança/Anotações.md
@@ -4,4 +4,16 @@ Herança é quando uma classe herda características comuns da classe pai.
 
 ## super
 
-super() executa o método da classe pai, ou seja, da classe que está sendo herdada.
+super() se refere sempre a classe pai, ele executa o método da classe pai, ou seja, da classe que está sendo herdada.
+
+## Substituindo métodos da classe pai
+
+A partir do momento que criamos uma função (método) com o mesmo nome da função que existe na classe pai, essa função da classe filho sobrescreverá a função da classe pai no momento de instanciação do objeto.
+
+Se quiser referenciar e usar a função da classe pai dentro de uma função da classe filha, use o super() dessa forma:
+
+```
+sayHi() {
+  super.sayhi();
+}
+```
--- Módulo 4 - Objetos e Orientação à objetos/7. Classes Herança/assets/js/script.js
+++ Módulo 4 - Objetos e Orientação à objetos/7. Classes Herança/assets/js/script.js
@@ -1,19 +1,43 @@
+// Classe pai
 class Person {
   age = 0;
 
   constructor (name) {
     this.name = name;
   }
+
+  sayHi() {
+    console.log(`${ this.name } diz Oi!`);
+  }
 }
 
-// classe que herda tudo de person
-// além das próprias características de student
+// classe que herda tudo de Person
+// além das próprias características de Student
 class Student extends Person {
   // constructor da classe student
+  // somente Student tem id, Person não tem
   constructor (name, id) {
     // name virá de Person
     super(name);
     this.id = id;
   }
+
+  // Sobrescrevendo a função da classe pai
+  sayHi() {
+    console.log(`${ this.name } é um estudante e diz olá`);
+  }
+
+  sayHello() {
+    // executando a função da classe pai
+    super.sayHi();
+  }
 }
 
+let p1 = new Student("Erivelton", 1);
+// como Student herda tudo de Person,
+// tenho acesso ao age
+p1.age = 33;
+console.log(`${ p1.name } tem ${ p1.age } anos e matrícula: #${ p1.id }`);
+
+p1.sayHi();
+p1.sayHello();
