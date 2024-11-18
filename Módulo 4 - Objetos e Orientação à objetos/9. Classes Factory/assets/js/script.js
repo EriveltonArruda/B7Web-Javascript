7afbbb02f720ea800fd08d0d43c8598a08d86fb1
Erivelton Rodrigues <eriveltonarruda3000@gmail.com>
2024-11-18 17:15:36 -0300

    Adicionando a Prática do Factory

--- Módulo 4 - Objetos e Orientação à objetos/9. Classes Factory/assets/js/script.js
+++ Módulo 4 - Objetos e Orientação à objetos/9. Classes Factory/assets/js/script.js
@@ -17,3 +17,22 @@ function createPerson(name, age) {
 let p1 = createPerson("Erivelton", 33);
 
 console.log(`${ p1.name } tem ${ p1.age } anos`);
+
+// Prática
+class Car {
+  color = "Branca";
+  constructor (brand, model) {
+    this.brand = brand;
+    this.model = model;
+  }
+}
+
+function createCar(brand, model, color) {
+  let c = new Car(brand, model, color);
+  c.color = "Cinza";
+  return c;
+}
+
+let c1 = createCar("Porsche", "811 Turbo");
+
+console.log(`Depois de muito esforço, consegui comprar minha ${ c1.brand } ${ c1.model } na cor ${ c1.color }`);
