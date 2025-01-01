b84d3d064af6ac9afd5c8abd4cfbd45e6628b982
Erivelton Rodrigues <eriveltonarruda3000@gmail.com>
2025-01-01 18:41:28 -0300

    Projeto luta Funcional - criando ataques

---
+++ Módulo 4 - Objetos e Orientação à objetos/24. Projeto Luta Funcional - Criando Ataques/Anotações.md
@@ -0,0 +1,1 @@
+Toda a lógica do projeto foi explicada no projeto feito com classes, aqui será somente transformado em funcional.
--- Módulo 4 - Objetos e Orientação à objetos/24. Projeto Luta Funcional - Criando Ataques/assets/js/functions.js
+++ Módulo 4 - Objetos e Orientação à objetos/24. Projeto Luta Funcional - Criando Ataques/assets/js/functions.js
@@ -52,3 +52,69 @@ const createBigMonster = () => {
     defense: 6
   }
 }
+
+const stage = {
+  // Pode retirar se quiser (é opcional)
+  fighter1: null,
+  fighter2: null,
+  fighter1El: null,
+  fighter2El: null,
+
+  // Responsável por iniciar o jogo (Passo 1)
+  start(fighter1, fighter2, fighter1El, fighter2El) {
+    this.fighter1 = fighter1;
+    this.fighter2 = fighter2;
+    this.fighter1El = fighter1El;
+    this.fighter2El = fighter2El;
+
+    // Evento do botão (Passo 2)
+    // fighter1 ataca fighter2
+    this.fighter1El.querySelector('.attackButton').addEventListener('click', () => this.doAttack(this.fighter1, this.fighter2));
+    // fighter2 ataca fighter1
+    this.fighter2El.querySelector('.attackButton').addEventListener('click', () => this.doAttack(this.fighter2, this.fighter1));
+
+    this.update();
+  },
+
+  // Passo 3
+  update() {
+    // Fighter 1
+    this.fighter1El.querySelector(".name").innerHTML = `${this.fighter1.name} - ${this.fighter1.life.toFixed(1)} HP`;
+    // Cálculo da barra de vida
+    let f1Pct = (this.fighter1.life / this.fighter1.maxLife) * 100;
+    this.fighter1El.querySelector(".bar").style.width = `${f1Pct}%`;
+    // Fighter 2
+    this.fighter2El.querySelector(".name").innerHTML = `${this.fighter2.name} - ${this.fighter2.life.toFixed(1)} HP`;
+    // Cálculo da barra de vida
+    let f2Pct = (this.fighter2.life / this.fighter2.maxLife) * 100;
+    this.fighter2El.querySelector(".bar").style.width = `${f2Pct}%`;
+  },
+
+  doAttack(attacking, attacked) {
+    // Verificando se tem alguém vivo
+    if (attacking.life <= 0 || attacked.life <= 0) {
+      console.log("Alguém está morto, não pode atacar");
+      return;
+    }
+
+    // Fator de ataque e defesa
+    const attackFactor = (Math.random() * 2).toFixed(2);
+    const defenseFactor = (Math.random() * 2).toFixed(2);
+
+    // usando os fatores
+    const actualAttack = attacking.attack * attackFactor;
+    const actualDefense = attacked.defense * defenseFactor;
+
+    // verificação para saber se atacou ou defendeu.
+    if (actualAttack > actualDefense) {
+      attacked.life -= actualAttack;
+      // Verificação para a vida não ficar negativa.
+      attacked.life = attacked.life < 0 ? 0 : attacked.life;
+      console.log(`${attacking.name} causou ${actualAttack.toFixed(2)} de dano em ${attacked.name}`)
+    } else {
+      console.log(`${attacked.name} conseguiu defender`);
+    }
+
+    this.update();
+  }
+}
