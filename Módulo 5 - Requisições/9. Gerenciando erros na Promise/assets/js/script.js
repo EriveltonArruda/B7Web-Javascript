7bc5a168abbccbd0fc4599575d7834fda1955a0a
Erivelton Rodrigues <eriveltonarruda3000@gmail.com>
2025-01-09 16:49:11 -0300

    .

---
+++ Módulo 5 - Requisições/9. Gerenciando erros na Promise/assets/js/script.js
@@ -0,0 +1,11 @@
+function clicou() {
+  fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      alert(`Título do primeiro post: ${ response[0].title }`);
+    });
+};
+
+document.querySelector("#botao").addEventListener("click", clicou);
