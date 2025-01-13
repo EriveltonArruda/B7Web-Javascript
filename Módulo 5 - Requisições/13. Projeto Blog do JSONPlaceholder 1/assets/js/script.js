1817041a92cc971cdc9f2e35de948826522fe01f
Erivelton Rodrigues <eriveltonarruda3000@gmail.com>
2025-01-13 20:09:47 -0300

    Projeto: Blog do JSONPlaceholder 1

---
+++ Módulo 5 - Requisições/13. Projeto Blog do JSONPlaceholder 1/assets/js/script.js
@@ -0,0 +1,25 @@
+async function readPosts() {
+  // Exibindo a mensagem na tela.
+  let postArea = document.querySelector(".posts");
+  postArea.innerHTML = 'Carregando...';
+
+  // Requisição.
+  let response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  let json = await response.json();
+
+  // Verificando se veio algum dado da API
+  if (json.length > 0) {
+    // Tira o nome carregando (limpa a div)
+    postArea.innerHTML = '';
+
+    for (let i in json) {
+      // colocando os posts na tela
+      let postHTML = `<div><h1>${json[i].title}</h1>${json[i].body}<hr></div>`;
+      postArea.innerHTML += postHTML;
+    }
+  } else {
+    postArea.innerHTML = 'Nenhum post para exibir';
+  }
+}
+
+readPosts();
