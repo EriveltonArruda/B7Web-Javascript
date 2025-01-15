666f3d0b85d0010fbf72a208e20a09c94643391d
Erivelton Rodrigues <eriveltonarruda3000@gmail.com>
2025-01-15 17:50:53 -0300

    Métodos de Array (2/4)

---
+++ Módulo 6 - Manipulações, Arrow, Ajax, etc/6. Métodos de Arrays (2-4)/Anotações.md
@@ -0,0 +1,10 @@
+## Parâmetros do splice()
+
+1. Qual é o index(item) que quer remover.
+2. Quantos itens quer remover.
+
+## reverse()
+
+Para usar o reverse e deixar os itens do array em ordem alfabética decrescente, preciso ordenar em ordem alfabética com o sort primeiro.
+
+Se usar somente o reverse sem o sort, ele vai simplesmente ordenar de forma decrescente.
---
+++ Módulo 6 - Manipulações, Arrow, Ajax, etc/6. Métodos de Arrays (2-4)/assets/js/script.js
@@ -0,0 +1,19 @@
+let lista = ['Ovo', 'Farinha', 'Corante', 'Massa'];
+let lista2 = ['Prato', 'Liquidificador', 'Forno'];
+// Remove o item do array com base no index e a quantidade de itens
+lista.splice(0, 1);
+console.log(lista);
+
+// Agrupa dois (ou mais) arrays em um só
+let junto = lista.concat(lista2);
+console.log(junto);
+
+// Ordena o array em ordem alfabética
+lista.sort();
+console.log(lista);
+
+
+lista.sort();
+// Ordena em ordem decrescente
+lista.reverse();
+console.log(lista);
