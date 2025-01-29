09ba029a618d120490777f894b48eb18bea9c66f
Erivelton Rodrigues <eriveltonarruda3000@gmail.com>
2025-01-29 20:46:37 -0300

    .

--- Módulo 8 - Desafio 7 Projetos em 7 Dias/Projeto 3 - Clima/script.js
+++ Módulo 8 - Desafio 7 Projetos em 7 Dias/Projeto 3 - Clima/script.js
@@ -1,6 +1,6 @@
 
 document.querySelector('.busca').addEventListener('submit', async (event) => {
-  event.preventDefault(); // Previnindo ação do formulário
+  event.preventDefault(); // Prevenindo ação do formulário
 
   let input = document.querySelector('#searchInput').value;
 
@@ -8,7 +8,7 @@ document.querySelector('.busca').addEventListener('submit', async (event) => {
     showWarning('Carregando...');
   }
 
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${ encodeURI(input) }&appid=861e25244eab2e37ccc70d1ba12e7af0&units=metric&lang=pt_br`;
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=861e25244eab2e37ccc70d1ba12e7af0&units=metric&lang=pt_br`;
 
   let results = await fetch(url); // fazendo a requisição
   let json = await results.json(); // transformando em objeto
