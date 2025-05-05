5f27eb7f8072e7eb31d955c5a3f43670664c5fb5
Erivelton Rodrigues <eriveltonarruda3000@gmail.com>
2025-05-05 16:54:27 -0300

    Adicionando comentários [parte 1]

--- Módulo 8 - Desafio 7 Projetos em 7 Dias/Projeto 4 - Jogo da velha/script.js
+++ Módulo 8 - Desafio 7 Projetos em 7 Dias/Projeto 4 - Jogo da velha/script.js
@@ -1,113 +1,141 @@
 // Dados iniciais
+// Cópia do tabuleiro
 let quadro = {
-    a1: '', a2: '', a3: '',
-    b1: '', b2: '', b3: '',
-    c1: '', c2: '', c3: ''
+  a1: '', a2: '', a3: '',
+  b1: '', b2: '', b3: '',
+  c1: '', c2: '', c3: ''
 };
+
+// saber se o jogo está rodando
 let playing = false;
 let vez = 'x';
+// mensagem (ganhou, perdeu, empate)
 let warning = '';
 
+// Função que reseta o jogo
 reset();
 
 // Eventos
 document.querySelector('.reset').addEventListener('click', reset);
+// percorre todos os itens
+document.querySelectorAll('.item').forEach((item) => {
+  // adiciona o evento de clique a cada um
+  item.addEventListener('click', (e) => {
+    // sabe em quem foi clicado (target contém o elemento clicado)
+    let loc = e.target.getAttribute('data-item');
 
-document.querySelectorAll('.item').forEach((item)=>{
-    item.addEventListener('click', (e) => {
-        let loc = e.target.getAttribute('data-item');
-        
-        if(playing && quadro[loc] === '') {
-            quadro[loc] = vez;
-            renderQuadro();
-            togglePlayer();
-        }
-    });
+    // adiciona o 'x' ou o 'o' no quadro vazio que foi clicado
+    if (playing && quadro[loc] === '') {
+      quadro[loc] = vez;
+      renderQuadro();
+      togglePlayer();
+    }
+  });
 });
 
 // Funções
+
 function reset() {
-    warning = '';
+  // reseta a mensagem
+  warning = '';
 
-    // definir a vez
-    let random = Math.floor(Math.random() * 2);
-    vez = random === 0 ? 'x' : 'o';
+  // definir a vez
+  let random = Math.floor(Math.random() * 2);
+  vez = random === 0 ? 'x' : 'o';
 
-    // resetar os quadros
-    for(let i in quadro) {
-        quadro[i] = '';
-    }
+  // resetar os quadros
+  for (let i in quadro) {
+    // acessando o objeto e zerando ele
+    quadro[i] = '';
+  }
 
-    // renderizar tudo
-    renderQuadro();
-    renderInfo();
+  // renderizar tudo
+  renderQuadro();
+  renderInfo();
 
-    playing = true;
+  playing = true;
 }
 
+// função que renderiza o quadro
 function renderQuadro() {
-    for(let i in quadro) {
-        let item = document.querySelector(`div[data-item=${i}]`);
-        if(quadro[i] !== '') {
-            item.innerHTML = quadro[i];
-        } else {
-            item.innerHTML = '';
-        }
+  // vai percorrer o quadro e vai verificar se tem algo preenchido em cada elemento, se tiver coloca no HTML, se não tiver, não coloca.
+  for (let i in quadro) {
+    let item = document.querySelector(`div[data-item=${ i }]`);
+    // se tem algo preenchido na memória
+    if (quadro[i] !== '') {
+      // preenche no HTML
+      item.innerHTML = quadro[i];
+    } else {
+      // caso não tenha, fica vazio
+      item.innerHTML = '';
     }
 
-    checkGame();
+    /* 
+    poderia fazer assim sem o if que funcionaria
+        item.innerHTML = quadro[i];
+    */
+  }
+
+  checkGame();
 }
 
 function renderInfo() {
-    document.querySelector('.vez').innerHTML = vez;
-    document.querySelector('.resultado').innerHTML = warning;
+  document.querySelector('.vez').innerHTML = vez;
+  document.querySelector('.resultado').innerHTML = warning;
 }
 
+// Função para trocar a vez
 function togglePlayer() {
-    vez = vez === 'x' ? 'o' : 'x';
-    renderInfo();
+  vez = vez === 'x' ? 'o' : 'x';
+  // exibindo na tela
+  renderInfo();
 }
 
+// Função para verificar se 'x' ou 'o' ganhou ou se deu empate
 function checkGame() {
-    if(checkWinnerFor('x')) {
-        warning = 'O "x" venceu';
-        playing = false;
-    } else if(checkWinnerFor('o')) {
-        warning = 'O "o" venceu';
-        playing = false;
-    } else if(isFull()) {
-        warning = 'Deu empate';
-        playing = false;
-    }
+  if (checkWinnerFor('x')) {
+    warning = 'O "x" venceu';
+    // finaliza o jogo para evitar que continue clicando
+    playing = false;
+  } else if (checkWinnerFor('o')) {
+    warning = 'O "o" venceu';
+    playing = false;
+  } else if (isFull()) {
+    warning = 'Deu empate';
+    playing = false;
+  }
 }
 
 function checkWinnerFor(i) {
-    let pos = [
-        'a1,a2,a3',
-        'b1,b2,b3',
-        'c1,c2,c3',
+  // possibilidades de vencer
+  let pos = [
+    'a1,a2,a3',
+    'b1,b2,b3',
+    'c1,c2,c3',
 
-        'a1,b1,c1',
-        'a2,b2,c2',
-        'a3,b3,c3',
+    'a1,b1,c1',
+    'a2,b2,c2',
+    'a3,b3,c3',
 
-        'a1,b2,c3',
-        'a3,b2,c1'
-    ];
+    'a1,b2,c3',
+    'a3,b2,c1'
+  ];
 
-    for(let w in pos) {
-        let pArray = pos[w].split(',');
-        let hasWon = pArray.every(option=>quadro[option] === i);
-        if(hasWon) return true;
-    }
+  // verifica se o player está com algumas dessas posições
+  for (let w in pos) {
+    // separando por vírgula
+    let pArray = pos[w].split(','); // a1, a2, a3
+    let hasWon = pArray.every(option => quadro[option] === i);
+    if (hasWon) return true;
+  }
 
-    return false;
+  return false;
 }
 function isFull() {
-    for(let i in quadro) {
-        if(quadro[i] === '') {
-            return false;
-        }
+  for (let i in quadro) {
+    if (quadro[i] === '') {
+      return false;
     }
-    return true;
+  }
+  return true;
 }
