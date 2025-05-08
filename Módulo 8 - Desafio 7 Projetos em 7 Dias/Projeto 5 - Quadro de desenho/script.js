a06eefc31a142952cb1969bc1e8bd95fb98c013c
Erivelton Rodrigues <eriveltonarruda3000@gmail.com>
2025-05-08 17:56:15 -0300

    Criando eventos e funções

---
+++ Módulo 8 - Desafio 7 Projetos em 7 Dias/Projeto 5 - Quadro de desenho/script.js
@@ -0,0 +1,87 @@
+// Initial Data
+
+// cor que está selecionada por padrão
+let currentColor = 'black';
+let canDraw = false;
+let mouseX = 0;
+let mouseY = 0;
+
+
+// selecionando o elemento canvas para poder escrever nele
+let screen = document.querySelector('#tela');
+let ctx = screen.getContext('2d'); // contexto dentro do canvas que permite que seja desenhado dentro dele
+
+/*
+Passo a passo para desenhar no canvas (são eventos)
+- Quando o click do mouse ABAIXAR (apertar), ative o modo desenho.
+- Quando o mouse se MOVER, se o modo desenho estiver ativado, desenhe.
+- Quando o click do mouse LEVANTAR (soltar o click), desative o modo desenho
+*/
+
+// EVENTOS
+
+// Como temos várias cores, vamos usar um foreach e colocar o evento em cada uma delas
+document.querySelectorAll('.colorArea .color').forEach(item => {
+  item.addEventListener('click', colorClickEvent);
+});
+
+// eventoS doS botões do mouse
+screen.addEventListener('mousedown', mouseDownEvent); // apertou o botão do mouse
+screen.addEventListener('mousemove', mouseMoveEvent); // movimento do mouse
+screen.addEventListener('mouseup', mouseUpEvent); // soltou o botão do mouse
+
+
+// FUNÇÕES
+
+// função da troca da cor
+function colorClickEvent(e) {
+  let color = e.target.getAttribute("data-color"); // verificando qual cor foi clicada
+  currentColor = color; // armazenando a cor que foi clicada
+
+  // tirando o active de quem está ativo (precisa tirar antes de adicionar)
+  document.querySelector('.color.active').classList.remove('active');
+  e.target.classList.add('active'); // adicionando o active em quem foi clicado.
+}
+
+// funções de movimento do mouse
+function mouseDownEvent(e) {
+  // ativar modo desenho
+  canDraw = true;
+  // pageX mostra a posição horizontal do mouse na tela
+  // pageY mostra a posição vertical do mouse na tela
+  // é necessário compensar a posição para saber a posição real
+  // offsetLeft é a distância do elemento para o fim da página na esquerda (horizontal)
+  mouseX = e.pageX - screen.offsetLeft;
+  mouseY = e.pageY - screen.offsetTop;
+}
+
+function mouseMoveEvent(e) {
+  if (canDraw) {
+    draw(e.pageX, e.pageY);
+  }
+}
+
+function mouseUpEvent() {
+  // desativar modo desenho
+  canDraw = false;
+}
+
+function draw(x, y) {
+  // pegando a posição do mouse na tela
+  let pointX = x - screen.offsetLeft;
+  let pointY = y - screen.offsetTop;
+
+  // fazendo o desenho
+  ctx.beginPath(); // inicia o processo de desenho
+  ctx.lineWidth = 5; // grossura da linha
+  ctx.lineJoin = "round"; // formato da linha (arredondada)
+  ctx.moveTo(mouseX, mouseY); // posição do mouse
+  ctx.lineTo(pointX, pointY);
+  ctx.closePath(); // fecha o processo de desenho
+  ctx.strokeStyle = currentColor; // colorindo a linha
+  ctx.stroke(); // finaliza todo o processo
+
+  // salvando a posição atual
+  mouseX = pointX;
+  mouseY = pointY;
+}
