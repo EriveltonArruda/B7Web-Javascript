278db203d03136f609fe7b3d51749dd64116aece
Erivelton Rodrigues <eriveltonarruda3000@gmail.com>
2025-05-20 17:20:27 -0300

    Projeto compra de Pizzas - parte 9

---
+++ Módulo 9 - Projeto Compra de Pizzas/Compra de Pizzas - parte 9/script.js
@@ -0,0 +1,101 @@
+let cart = []; // carrinho de compras
+let modalKey = 0; // mostra qual é a pizza selecionada (vai ser usada no carrinho)
+let modalQt = 1;
+
+// Funções auxiliares
+const c = (el) => document.querySelector(el); // retorna o item
+const cs = (el) => document.querySelectorAll(el); // retorna um array com os itens
+
+// Listagem das pizzas
+pizzaJson.map((item, index) => {
+  let pizzaItem = c('.models .pizza-item').cloneNode(true);
+
+  pizzaItem.setAttribute('data-key', index);
+  pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+  pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+  pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+  pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${ item.price.toFixed(2) }`;
+
+  // evento de clique para abrir o Modal
+  pizzaItem.querySelector('a').addEventListener('click', (e) => {
+    e.preventDefault();
+    let key = e.target.closest('.pizza-item').getAttribute('data-key');
+    modalQt = 1;
+    modalKey = key; // mostra qual é a pizza
+
+
+    c('.pizzaBig img').src = pizzaJson[key].img;
+    c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+    c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+    c('.pizzaInfo--actualPrice').innerHTML = `R$ ${ pizzaJson[key].price.toFixed(2) }`;
+
+    c('.pizzaInfo--size.selected').classList.remove('selected');
+
+    cs('.pizzaInfo--size').forEach((size, sizeIndex) => {
+      if (sizeIndex === 2) {
+        size.classList.add('selected');
+      }
+      size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
+    });
+
+    c('.pizzaInfo--qt').innerHTML = modalQt;
+
+    c('.pizzaWindowArea').style.opacity = 0;
+    c('.pizzaWindowArea').style.display = 'flex';
+    setTimeout(() => {
+      c('.pizzaWindowArea').style.opacity = 1;
+    }, 200);
+  })
+
+  c('.pizza-area').append(pizzaItem)
+});
+
+// Eventos do Modal
+function closeModal() {
+  c('.pizzaWindowArea').style.opacity = 0;
+  setTimeout(() => {
+    c('.pizzaWindowArea').style.display = 'none';
+  }, 500);
+}
+
+cs('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item) => {
+  item.addEventListener('click', closeModal);
+});
+
+c('.pizzaInfo--qtmenos').addEventListener('click', () => {
+  if (modalQt > 1) {
+    modalQt--;
+    c('.pizzaInfo--qt').innerHTML = modalQt;
+  }
+});
+
+c('.pizzaInfo--qtmais').addEventListener('click', () => {
+  modalQt++;
+  c('.pizzaInfo--qt').innerHTML = modalQt;
+});
+
+cs('.pizzaInfo--size').forEach((size, sizeIndex) => {
+  size.addEventListener('click', (e) => {
+    c('.pizzaInfo--size.selected').classList.remove('selected');
+    size.classList.add('selected');
+  });
+});
+
+// ação do Carrinho de compras
+c('.pizzaInfo--addButton').addEventListener('click', () => {
+  // informações necessárias do carrinho;
+  // Qual a pizza? (modalKey)
+  // Qual o tamanho? (size)
+  let size = parseInt(c('.pizzaInfo--size.selected').getAttribute('data-key'));
+  // Quantas pizzas? (modalQt)
+
+  // adicionando no carrinho
+  cart.push({
+    id: pizzaJson[modalKey].id,
+    size,
+    qt: modalQt
+  });
+
+  // fechando modal após adicionar no carrinho
+  closeModal();
+});
