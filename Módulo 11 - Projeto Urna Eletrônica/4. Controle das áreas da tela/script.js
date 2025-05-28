f49b32a63cb9116a82378df621e3d72e357ac026
Erivelton Rodrigues <eriveltonarruda3000@gmail.com>
2025-05-28 11:47:56 -0300

    Projeto urna Eletrônica: Controle das áreas da tela

---
+++ Módulo 11 - Projeto Urna Eletrônica/4. Controle das áreas da tela/script.js
@@ -0,0 +1,7 @@
+// Controle do conteúdo da urna (texto e imagens)
+let seuVotoPara = document.querySelector('.d-1-1 span');
+let cargo = document.querySelector('.d-1-2 span'); // Prefeito / vereador
+let descricao = document.querySelector('.d-1-4'); // descrição do candidato
+let aviso = document.querySelector('.d-2'); // parte inferior da urna
+let lateral = document.querySelector('.d-1-right'); // área das fotos
+let numeros = document.querySelector('.d-1-3'); // Quadrados dos números
